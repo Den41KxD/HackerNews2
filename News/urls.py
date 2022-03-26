3ff96ef55804986fa2c371ad8aa79f34581efd85
Den41KxD <den41kxd67@gmail.com>
2022-03-26 03:21:54 +0200

    fix

--- News/urls.py
+++ News/urls.py
@@ -15,12 +15,12 @@ router.register('comment', CommentViewSet)
 urlpatterns = [
 
     path('', PostView.as_view(), name='index'),
-    path('login/', Login.as_view(), name='login'),
+    path('login/', csrf_exempt(Login.as_view()), name='login'),
     path('register/', Register.as_view(), name='register'),
     path('logout/', Logout.as_view(), name='logout'),
-    path('comment/<int:pk>/', CommentCreatedView.as_view(), name='comment'),
-    path('update/<int:pk>/', UpvotesUpdateView.as_view(), name='update'),
-    path('createpost/', CreatePostView.as_view(), name='createPost'),
+    path('comment/<int:pk>/', csrf_exempt(CommentCreatedView.as_view()), name='comment'),
+    path('update/<int:pk>/', csrf_exempt(UpvotesUpdateView.as_view()), name='update'),
+    path('createpost/', csrf_exempt(CreatePostView.as_view()), name='createPost'),
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token)
 
